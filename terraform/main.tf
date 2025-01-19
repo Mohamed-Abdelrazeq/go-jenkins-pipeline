cf15651612d1e4732e2da8e1374f842c9c37b62d
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2025-01-19 03:28:25 +0200

    test-deploy added docker on deployment server boot

--- terraform/main.tf
+++ terraform/main.tf
@@ -81,6 +81,15 @@ resource "aws_instance" "deployment_server" {
     Name = "deployment-server"
   }
   
+   user_data = <<-EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum install -y docker
+    sudo service docker start
+    sudo groupadd docker
+    sudo usermod -aG docker ec2-user
+    su -s ec2-user
+    EOF
 }
 
 # Deployment Elastic IP
